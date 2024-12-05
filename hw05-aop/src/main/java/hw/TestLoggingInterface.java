3426cbb83aec91b98ae2e428d55e40d589a6cbba
owner_va <owner_va@>
2024-12-05 23:17:39 +0300

    hw05

---
+++ hw05-aop/src/main/java/hw/TestLoggingInterface.java
@@ -0,0 +1,11 @@
+package hw;
+
+public interface TestLoggingInterface {
+
+    public void calculation(int param);
+
+    public void calculation(int param, int paramTwo);
+
+    public void calculation(int param, int paramTwo, int paramThree);
+
+}
