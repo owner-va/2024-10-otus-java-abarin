5e011f35f3b7339c3c2cb3b3c5fc42ffbec23d05
owner_va <owner_va@>
2025-01-16 17:58:12 +0300

    hw07 patterns refactor HomeWork.java

--- hw07-patterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-patterns/src/main/java/ru/otus/HomeWork.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.handler.ComplexProcessor;
 import ru.otus.listener.ListenerPrinterConsole;
+import ru.otus.listener.homework.HistoryListener;
 import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
 import ru.otus.processor.homework.EvenSecondProcessor;
 import ru.otus.processor.homework.SwapProcessor;
 
@@ -21,16 +23,23 @@ public class HomeWork {
         var complexProcessor = new ComplexProcessor(processors, ex -> {
         });
         var listenerPrinter = new ListenerPrinterConsole();
+        var historyListener = new HistoryListener();
         complexProcessor.addListener(listenerPrinter);
+        complexProcessor.addListener(historyListener);
 
+        List<String> stringsField13 = List.of("one", "two", "three");
+        ObjectForMessage objectForMessage = new ObjectForMessage();
+        objectForMessage.setData(stringsField13);
         var message = new Message.Builder(1L)
                 .field11("field11")
                 .field12("field12")
+                .field13(objectForMessage)
                 .build();
 
         var result = complexProcessor.handle(message);
         logger.info("result:{}", result);
 
         complexProcessor.removeListener(listenerPrinter);
+        complexProcessor.removeListener(historyListener);
     }
 }
